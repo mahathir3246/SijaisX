747165a052ccf9fd98794da4061d6236bf25a4ef
Irfan Hossain <irfan.hossain@aalto.fi>
2025-07-20 16:17:20 +0300

    route made

---
+++ backend/src/subcoord_functions/subcoord_routes.py
@@ -0,0 +1,14 @@
+from Flask import Blueprint, request, jsonify
+from .subcoord_queries import add_sub_to_volunteers_in_school
+
+subcoord_bp = Blueprint("subcoord_bp", __name__)
+
+@subcoord_bp.route("/api/substitute_coordinator/<teacher_ID>/add_substitute_to_list", methods=["POST"])
+def add_substitute_to_list(teacher_ID):
+    data = request.json
+    subsitute_ID = data.get("substitute_ID")
+    result = add_sub_to_volunteers_in_school(teacher_ID, subsitute_ID)
+
+    if not result["success"]:
+        return jsonify(result), 400
+    return jsonify(result), 200
