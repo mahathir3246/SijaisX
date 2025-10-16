8afe06bd984d0099031e35580a4c51e9fa8b466b
Irfan Hossain <irfan.hossain@aalto.fi>
2025-10-16 16:49:42 +0300

    tested

--- backend/src/db.py
+++ backend/src/db.py
@@ -5,4 +5,5 @@ DATABASE_NAME = "database.db"
 def get_db_connection():
     conn = sqlite3.connect(DATABASE_NAME)
     conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")  # Enable foreign key support
     return conn
