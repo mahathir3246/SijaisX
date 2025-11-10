9a83994b54f3379e250da26cbcaf9338e423c12f
mahathir3246 <mahathirmohammad112@gmail.com>
2025-11-10 16:59:18 +0200

    LogInUI done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,9 @@ import FullHomePage from "./components/HomePage/FullHomePage";
 import FullSijaisPage from "./components/Subpage/FullSubPage";
 import FullOpePage from "./components/TeacherPage/FullOpePage";
 import RegisterPage from "./components/Register/registerpage"
-import LogIn from './components/Login/login';
+import Login from './components/Login/loginForm';
 import ProtectedRoute from './components/Login/ProtectedRoute';
+import TeacherNewPage from "./components/TeacherNew/FullOpePage";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<FullHomePage/>} /> {/* Homepage content */}
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LogIn />} />
+        <Route path="/login" element={<Login />} />
 
         <Route
           path="/sijaisille"
@@ -31,6 +32,15 @@ function App() {
             <ProtectedRoute
               requiredRole='teacher'
               pageElemenTtoShow = {<FullOpePage/>}/>
+          }
+        />
+
+        <Route
+          path="/opettajille/new"
+          element={
+            <ProtectedRoute
+              requiredRole='teacher'
+              pageElemenTtoShow = {<TeacherNewPage/>}/>
           }
         />
       </Routes>
---
+++ frontend/src/components/TeacherNew/Navbar/teacherPageNavBar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Link } from 'react-router-dom';
+import navigationStyles from "../../../scss_stylings/navbar.module.scss";
+import NavDropdownMobile from "./TeacherNavDropdownmobile";
+import NavigationBarComponents from "./teacherNavigationBarComponents";
+
+const NavigationBar = () => {
+  return (
+    <div className={navigationStyles.headerContainer}>
+      <div className={navigationStyles.headerSubContainer}>
+        <NavDropdownMobile />
+        <Link className={navigationStyles.logoContainer} to="/">
+          <div className={navigationStyles.logoSmall}>
+            SijaisX 
+          </div>
+          <div className={navigationStyles.logoBig}>
+            SijaisX
+          </div>
+        </Link>
+        <NavigationBarComponents />
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
